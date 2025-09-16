08084d56dc1a97e951d1722e0711d5c38d8ff3e5
Arshad-005 <10614941@bradfordcollege.ac.uk>
2025-09-16 14:06:15 +0100

    Create arshads2.py

---
+++ arshads2.py
@@ -0,0 +1,9 @@
+numbers = [12, 5, 8, 22, 7, 19, 30, 4, 10, 16]
+
+print("Largest:", max(numbers))
+print("smallest:", min(numbers))
+print("Average:", sum(numbers) / len(numbers))
+
+num_set = set(numbers)
+print("Original list:", numbers)
+print("Converted set:", num_set) 
