eeba3a89ec10d4e7760ea3e5c116fa9eee84a6ae
Arshad-005 <10614941@bradfordcollege.ac.uk>
2025-09-16 14:27:12 +0100

    Create arshads3.py

---
+++ arshads3.py
@@ -0,0 +1,11 @@
+def analyse_sentence(sentence):
+    words = sentence.lower().split()
+    total_words = len(words)
+    unique_words = len(set(word))
+    most_common = Counter(words).most_common(1)[0]
+
+    return total_words, unique_words, most_common
+sentence = "python is great and python is fun and useful"
+total, unique, common = analyse_sentence(sentence)
+
+print("total words:", total)
